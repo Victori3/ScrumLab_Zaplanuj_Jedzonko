0e722b84ede88fa9637a932fb70e259e107bf462
Viktoriia Strilchuk <strilchuk.victoria@gmail.com>
2019-05-15 18:29:50 +0200

    dashboard

---
+++ development/js/dashboard.js
@@ -0,0 +1,33 @@
+//dodaj przepis
+var recipe = document.querySelector(".important-info");
+var icon = recipe.querySelector(".recipe");
+icon.addEventListener("click", function () {
+    window.open("../recipes.html");
+});
+
+//dodaj plan
+var plan = recipe.querySelector(".plan")
+plan.addEventListener("click", function () {
+    window.open("../schedules.html");
+});
+
+
+
+//nr tygodnia
+// var weekNr = document.querySelector("caption");
+// console.log(weekNr);
+// for (var i = 1; i <= 52; i++) {
+//     console.log = `Twój plan na ${i} tydzień:`;
+// };
+
+
+
+//strzałki na dole
+var arrowLeft = document.querySelector(".left");
+var arrowRight = document.querySelector(".right");
+arrowLeft.querySelector("i").addEventListener("click", function () {
+    ;
+});;
+arrowRight.querySelector("i").addEventListener("click", function () {
+    ;
+});;
